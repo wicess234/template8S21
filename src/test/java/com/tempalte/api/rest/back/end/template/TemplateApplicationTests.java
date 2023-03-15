fb58467788d760ca882d266afe69d119cbda82b7
wicess_234 <wicess_220389hotmail.com>
2023-03-15 11:21:18 -0600

    first commit

---
+++ src/test/java/com/tempalte/api/rest/back/end/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tempalte.api.rest.back.end.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
